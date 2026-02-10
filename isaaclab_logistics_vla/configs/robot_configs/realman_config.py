b5e5ff36eb11d6ca8cdb6e3aef66a42df650262a
RidenShogunei <chenjinxu2003@163.com>
2026-02-10 17:03:34 +0800

    修改

--- isaaclab_logistics_vla/configs/robot_configs/realman_config.py
+++ isaaclab_logistics_vla/configs/robot_configs/realman_config.py
@@ -42,7 +42,7 @@ class RealmanFrankaEE(ArticulationCfg):
                     "left_right_joint":0.04,
                     "right_right_joint":0.04,
                     "right_left_joint":0.04,
-                    "platform_joint": 0.8},  # 添加平台关节的初始位置（你的修改）
+                    "platform_joint": 0.8},  # 添加平台关节的初始位置
             pos=(-0.71, 0, 0.216),
             rot= euler2quat('z',-90)    #正对x轴正方向
         )
